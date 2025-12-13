664127852111bcf77bf26d10de13b223805af346
sedavbotero <sdboterol@protonmail.com>
2025-12-13 11:55:57 +0100

    Initial commit

---
+++ modifiedRandomForest.py
@@ -0,0 +1,12 @@
+from sklearn.tree import DecisionTreeRegressor
+
+
+class ModifiedRandomForest:
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
