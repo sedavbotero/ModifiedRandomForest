907461f54b3a55da93940e423afcad6667f07a81
sedavbotero <sdboterol@protonmail.com>
2025-12-13 12:29:25 +0100

    Made random forest skeleton

--- modifiedRandomForest.py
+++ modifiedRandomForest.py
@@ -1,12 +1,26 @@
 from sklearn.tree import DecisionTreeRegressor
+import numpy as np
+import pandas as pd
+
+
+def sample_data(X: pd.DataFrame, y: pd.Series) -> tuple[pd.DataFrame, pd.Series]:
+    pass
 
 
 class ModifiedRandomForest:
-    def __init__(self):
+    def __init__(self, n_estimators, **kwargs):
+        self.n_estimators = n_estimators
+        self.kwargs = kwargs
+        self.estimators = []
         pass
 
-    def fit(self, X, y):
+    def fit(self, X: pd.DataFrame, y: pd.Series) -> None:
+        for _ in range(self.n_estimators):
+            X_sampled, y_sampled = sample_data(X, y)
+            tree = DecisionTreeRegressor(**self.kwargs)
+            tree.fit(X_sampled, y_sampled)
+            self.estimators.append(tree)
         pass
 
-    def predict(self, X):
+    def predict(self, X: pd.DataFrame) -> pd.Series:
         pass
