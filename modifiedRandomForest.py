c696f0248ccc8970f4585b4d265906f3cf3895bc
sedavbotero <sdboterol@protonmail.com>
2025-12-13 13:16:23 +0100

    Made sampling function

--- modifiedRandomForest.py
+++ modifiedRandomForest.py
@@ -3,8 +3,20 @@ import numpy as np
 import pandas as pd
 
 
-def sample_data(X: pd.DataFrame, y: pd.Series) -> tuple[pd.DataFrame, pd.Series]:
-    pass
+def sample_data(
+    X: pd.DataFrame,
+    y: pd.Series,
+    number_of_rows: int,
+    number_of_columns: int,
+    probs=None,
+) -> tuple[pd.DataFrame, pd.Series]:
+    rows = np.random.choice(
+        np.arange(X.shape[0]), number_of_rows, replace=True, p=probs
+    )
+    cols = np.random.choice(np.arange(X.shape[1]), number_of_columns, replace=False)
+    X_sampled = X.iloc[rows, cols]
+    y_sampled = y.iloc[rows]
+    return X_sampled, y_sampled
 
 
 class ModifiedRandomForest:
@@ -12,15 +24,15 @@ class ModifiedRandomForest:
         self.n_estimators = n_estimators
         self.kwargs = kwargs
         self.estimators = []
-        pass
 
     def fit(self, X: pd.DataFrame, y: pd.Series) -> None:
         for _ in range(self.n_estimators):
-            X_sampled, y_sampled = sample_data(X, y)
+            X_sampled, y_sampled = sample_data(
+                X, y, np.sqrt(X.shape[0]), np.sqrt(X.shape[1])
+            )
             tree = DecisionTreeRegressor(**self.kwargs)
             tree.fit(X_sampled, y_sampled)
             self.estimators.append(tree)
-        pass
 
     def predict(self, X: pd.DataFrame) -> pd.Series:
         pass
