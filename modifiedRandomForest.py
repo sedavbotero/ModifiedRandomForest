284c6ed096ca1a4ccc66505b3f360b42c3d5e604
starmann90 <marcinstr@gmail.com>
2025-12-20 19:26:21 +0100

    1. Changing row selection from Bagging to Bootstrapping 2. Weighting mechanism – instead of the arithmetic mean,  Softmax is now used on (-errors) 3. Added a new score method that returns the R2 coefficient

--- modifiedRandomForest.py
+++ modifiedRandomForest.py
@@ -2,8 +2,9 @@ from sklearn.tree import DecisionTreeRegressor
 import numpy as np
 import pandas as pd
 from sklearn.datasets import load_diabetes
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.ensemble import RandomForestRegressor
+from scipy.special import softmax
 
 
 def sample_data(
@@ -28,20 +29,36 @@ class ModifiedRandomForest:
         self.estimator_features = []
 
     def fit(self, X: pd.DataFrame, y: pd.Series) -> None:
+
+        self.estimators = []
+        self.estimator_features = []
+        errors = []
+
+        n_features_total = X.shape[1]
+        n_features_to_select = int(np.ceil(np.sqrt(n_features_total)))
+        n_rows_to_select = X.shape[0]
+
         for _ in range(self.n_estimators):
             X_sampled, y_sampled = sample_data(
                 X,
                 y,
                 # TODO: określić jak dużo kolumn wybierać i jak dużo wierszy
-                # np.ceil(np.sqrt(X.shape[0]), casting="unsafe", dtype=np.int32),
-                # np.ceil(np.sqrt(X.shape[1]), casting="unsafe", dtype=np.int32),
-                X.shape[0],
-                X.shape[1],
+                number_of_rows=n_rows_to_select,
+                number_of_columns=n_features_to_select,
             )
             tree = DecisionTreeRegressor(**self.kwargs)
             tree.fit(X_sampled, y_sampled)
             self.estimators.append(tree)
-            self.estimator_features.append(X_sampled.columns.to_numpy())
+
+            features = X_sampled.columns.to_numpy()
+            self.estimator_features.append(features)
+
+            predictions_train = tree.predict(X[features])
+            mse = mean_squared_error(y, predictions_train)
+            errors.append(mse)
+
+        errors_array = np.array(errors)
+        self.weights = softmax(-errors_array)
 
     def predict(self, X: pd.DataFrame) -> pd.Series:
         predictions = np.zeros(X.shape[0])
@@ -49,6 +66,9 @@ class ModifiedRandomForest:
             predictions += tree.predict(X.loc[:, features])
         return predictions / self.n_estimators
 
+    def score(self, X: pd.DataFrame, y: pd.Series) -> float:
+        y_pred = self.predict(X)
+        return r2_score(y, y_pred)
 
 ## Proste sprawdzenie
 # if __name__ == "__main__":
